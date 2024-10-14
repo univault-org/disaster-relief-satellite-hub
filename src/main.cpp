d625d10f7e64742d9bae2bca7578ce32c64726f9
Swan Family <swanfamily@test.local>
2024-10-14 13:08:18 -0600

    Initial commit for Disaster Relief Satellite Hub

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Disaster Relief Satellite Hub" << std::endl;
+    // TODO: Add initialization and main program logic here
+    return 0;
+}
